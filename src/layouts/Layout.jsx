14e50d21474731965debef32693fd460720cad67
Tosinfamzy <famurewatosin@gmail.com>
2023-07-03 18:02:53 +0100

    learning nested routes

--- src/layouts/Layout.jsx
+++
@@ -1,3 +0,0 @@
-export default function Layout() {
-	return <>{/* navigation goes here */}</>;
-}
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,20 @@ import './index.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans';
-// import VanDetail from './pages/VanDetail';
+import VanDetail from './pages/VanDetail';
+
+import '../server';
+import Layout from './assets/components/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
 		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='/about' element={<About />} />
-			<Route path='/vans' element={<Vans />} />
-			{/* <Route path='/vans/:id' element={<VanDetail />} /> */}
+			<Route element={<Layout />}>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/vans' element={<Vans />} />
+				<Route path='/vans/:id' element={<VanDetail />} />
+			</Route>
 		</Routes>
 	</BrowserRouter>
 );
